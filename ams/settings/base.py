99ce1b6303a3806b6ceab314e269634788be8151
vedghimi2054 <ghimires2054@gmail.com>
2025-03-07 16:51:08 +0545

    remove comment

--- ams/settings/base.py
+++ ams/settings/base.py
@@ -14,13 +14,8 @@ load_dotenv()
 
 
 class BaseSettings(Configuration):
-    # Load the SECRET_KEY from the environment variable
-    SECRET_KEY = os.getenv('SECRET_KEY', 'default-secret-key')  # Provide a fallback if not set
-
-    # SECURITY WARNING: don't run with debug turned on in production!
+    SECRET_KEY = os.getenv('SECRET_KEY', 'default-secret-key')
     DEBUG = os.getenv('DEBUG', 'True') == 'True'
-
-    # ALLOWED_HOSTS: You can specify multiple allowed hosts separated by commas
     ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
     INSTALLED_APPS = InstalledAppSettings.INSTALLED_APPS
@@ -32,19 +27,13 @@ class BaseSettings(Configuration):
     SIMPLE_JWT = AuthSettings.SIMPLE_JWT
 
     ROOT_URLCONF = 'core.urls'
-    # database is overloaded
+
     DATABASES = DatabaseURLValue()
 
     TEMPLATES = TemplatesSettings.TEMPLATES
 
     WSGI_APPLICATION = 'core.wsgi.application'
-
-    # Password validation
-    # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
     AUTH_PASSWORD_VALIDATORS = PasswordValidatorSettings.AUTH_PASSWORD_VALIDATORS
-
-    # Internationalization
-    # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
     LANGUAGE_CODE = 'en-us'
 
@@ -54,12 +43,6 @@ class BaseSettings(Configuration):
 
     USE_TZ = True
 
-    # Static files (CSS, JavaScript, Images)
-    # https://docs.djangoproject.com/en/5.1/howto/static-files/
-
     STATIC_URL = 'static/'
-
-    # Default primary key field type
-    # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
     DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
