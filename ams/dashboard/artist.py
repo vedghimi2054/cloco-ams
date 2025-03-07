4f09fe87ef26a3b2b91b8cdc609143c2efd8ad64
vedghimi2054 <ghimires2054@gmail.com>
2025-03-07 17:29:58 +0545

    refactor

--- ams/dashboard/artist.py
+++ ams/dashboard/artist.py
@@ -1,17 +1,10 @@
-from rest_framework import serializers
 from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 
 from artist.models import Artist
 
 
-class ArtistCountSerializer(serializers.Serializer):
-    total_artists = serializers.IntegerField()
-
-
 class ArtistCountView(GenericAPIView):
-    serializer_class = ArtistCountSerializer
-
     def get(self, request, *args, **kwargs):
         total_artists = Artist.objects.count()
-        return Response(self.get_serializer({'total_artists': total_artists}).data)
+        return Response({'total_artists': total_artists})
--- ams/dashboard/music.py
+++ ams/dashboard/music.py
@@ -1,17 +1,10 @@
-from rest_framework import serializers
 from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 
 from music.models import Music
 
 
-class MusicCountSerializer(serializers.Serializer):
-    total_music = serializers.IntegerField()
-
-
 class MusicCountView(GenericAPIView):
-    serializer_class = MusicCountSerializer
-
     def get(self, request, *args, **kwargs):
         total_music = Music.objects.count()
-        return Response(self.get_serializer({'total_music': total_music}).data)
+        return Response({'total_music': total_music})
